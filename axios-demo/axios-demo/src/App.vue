0b7f19a6d1b7b5af996bbae90cc8fcff22353c21
wuyuxin2 <564791604@qq.com>
2020-03-07 14:48:46 +0800

    axios rename

---
+++ axios-demo/axios-demo/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    {{ info }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      info: null
+    }
+  },
+  mounted() {
+    const axios = require('axios').default;
+    axios
+      .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+      .then(response => (this.info = response))
+  },
+  components: {}
+};
+</script>
+
+<style>
+</style>
